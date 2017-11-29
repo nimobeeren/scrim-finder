82cf95140414634250e11b7951e1dc430d72676f
Nimo Beeren <nimobeeren@gmail.com>
2017-11-29 19:54:58 +0100

    Initial color scheme

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import Post from './Post';
 
-import '../../styles/normalize.css'
+import '../../styles/normalize.css';
+import '../../styles/main.css';
 
 export default class App extends Component {
 	render() {
