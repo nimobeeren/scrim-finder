311b050a185e8ea8788bf7fc6a8c8f6a5c432ef1
Nimo Beeren <nimobeeren@gmail.com>
2017-12-06 21:49:41 +0100

    Limit navbar width and improve breakpoints

--- src/js/components/NavBar.js
+++ src/js/components/NavBar.js
@@ -6,12 +6,16 @@ import './../../styles/components/NavBar.css';
 
 const NavBar = () => (
 	<nav>
-		<div className="nav__left">
-			<h1 className="nav__item">CSGO Scrim Finder</h1>
-		</div>
-		<div className="nav__right">
-			<Button className="btn btn--nav" label="Log in" onClick={() => alert("Not yet implemented")}/>
-			<Button className="btn btn--secondary btn--about" label="" onClick={() => alert("Not yet implemented")}/>
+		<div className="nav__content">
+			<h1 className="nav__group">CSGO Scrim Finder</h1>
+			<div className="nav__group">
+				<div className="nav__item">
+					<Button className="btn btn--secondary btn--about" label="" onClick={() => alert("Not yet implemented")}/>
+				</div>
+				<div className="nav__item">
+					<Button className="btn btn--nav" label="Log in" onClick={() => alert("Not yet implemented")}/>
+				</div>
+			</div>
 		</div>
 	</nav>
 );
