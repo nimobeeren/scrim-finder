560a6bf9d8787ace7cfd79469543916599e82cc0
Nimo Beeren <nimobeeren@gmail.com>
2017-12-06 17:18:51 +0100

    Add basic functionality to GO button

--- src/js/components/PostActions.js
+++ src/js/components/PostActions.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 import Button from './Button';
+import '../../styles/components/PostActions.css';
 
+
+function handleClick(e) {
+	const feedback = e.target.parentNode.querySelector('.post-actions__feedback');
+	feedback.style.display = "inline-block";
+}
 
 const PostActions = () => (
 	<div>
-		<Button className="btn" label="GO" onClick={() => alert("Not yet implemented")}/>
+		<Button className="btn" label="GO" onClick={handleClick}/>
+		<div className="post-actions__feedback">Request sent</div>
 	</div>
 );
 
