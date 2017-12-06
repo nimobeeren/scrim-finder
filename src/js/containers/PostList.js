565602819e3a816ec41f9cdbc4727d567c208dca
Nimo Beeren <nimobeeren@gmail.com>
2017-12-06 22:13:42 +0100

    Give feedback when post list is empty

--- src/js/containers/PostList.js
+++ src/js/containers/PostList.js
@@ -5,21 +5,32 @@ import Post from '../components/Post';
 import '../../styles/containers/PostList.css';
 
 
+function renderPosts(posts) {
+	if (posts.length > 0) {
+		return posts.map((post, i) =>
+			<Post
+				key={'post' + i}
+				teamName={post.teamName}
+				level={post.level}
+				maps={post.maps}
+				server={post.server}
+				created={post.created}/>
+		);
+	} else {
+		return (
+			<div className="post-list__empty">
+				No matching posts found <br/>
+				Please adjust your filters
+			</div>
+		)
+	}
+}
+
 class PostList extends Component {
 	render() {
 		return (
 			<div className="post-list">
-				{
-					this.props.posts.map((post, i) =>
-						<Post
-							key={'post' + i}
-							teamName={post.teamName}
-							level={post.level}
-							maps={post.maps}
-							server={post.server}
-							created={post.created}/>
-					)
-				}
+				{renderPosts(this.props.posts)}
 			</div>
 		);
 	}
