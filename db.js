48252a7882d3f5f8caeca6c3a20d45640fe97dfc
Nimo Beeren <nimobeeren@gmail.com>
2017-12-14 13:34:02 +0100

    Connect to database only when required

--- db.js
+++ db.js
@@ -1,14 +1,21 @@
 const {MongoClient} = require('mongodb');
-const uri = 'mongodb://dbuser:35dJ1G1BEc0A@cluster0-shard-00-00-ii3ed.mongodb.net:27017,cluster0-shard-00-01-ii3ed.mongodb.net:27017,cluster0-shard-00-02-ii3ed.mongodb.net:27017/db?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
-const dbName = 'scrim-finder-db';
+
+let db;
+async function connect() {
+	const uri = 'mongodb://dbuser:35dJ1G1BEc0A@cluster0-shard-00-00-ii3ed.mongodb.net:27017,cluster0-shard-00-01-ii3ed.mongodb.net:27017,cluster0-shard-00-02-ii3ed.mongodb.net:27017/db?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+	const dbName = 'scrim-finder-db';
+
+	const client = await MongoClient.connect(uri);
+	console.log("Connected to db");
+	return client.db(dbName);
+}
 
 module.exports = {
 	getPosts: async function () {
-		const client = await MongoClient.connect(uri);
-		const db = client.db(dbName);
+		if (!db) {
+			db = await connect();
+		}
 		const collection = db.collection('posts');
-		const result = await collection.find().toArray();
-		client.close();
-		return result;
+		return collection.find().toArray();
 	}
 };
