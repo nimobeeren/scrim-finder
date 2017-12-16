39336e2711158116e7fb18000175785cc45a970d
Nimo Beeren <nimobeeren@gmail.com>
2017-12-16 21:11:35 +0100

    Split up API routes into separate file

---
+++ routes/api.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db/db');
+
+// Use Express middleware to parse JSON requests
+router.use('/posts', express.json());
+
+router.route('/posts')
+	.get(async (req, res) => {
+		// Get encoded query string from URL
+		const query = req.query.filters;
+
+		// Decode and parse query as JSON
+		let filters = null;
+		if (query) {
+			try {
+				filters = JSON.parse(decodeURIComponent(query));
+			} catch (e) {
+				if (e instanceof SyntaxError) {
+					res.status(400).send("Bad filters parameter");
+				} else {
+					throw e;
+				}
+			}
+		}
+
+		// Get posts that match filters from database
+		const posts = await db.getPosts(filters);
+		res.send(posts);
+	})
+	.put(async (req, res) => {
+		try {
+			await db.createPost(req.body);
+			res.status(200).send("Post created");
+		} catch (e) {
+			res.status(500).send("Server error when creating post");
+		}
+	});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,47 +1,12 @@
 const express = require('express');
 const path = require('path');
+const api = require('./routes/api');
 const app = express();
-const db = require('./db/db');
 
+// Serve static client build
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-// Use Express middleware to parse JSON requests
-app.use('/api/posts', express.json());
-
-app.get('/api/test', (req, res) => {
-	res.send("Hello world!");
-});
-
-app.get('/api/posts', async (req, res) => {
-	// Get encoded query string from URL
-	const query = req.query.filters;
-
-	// Decode and parse query as JSON
-	let filters = null;
-	if (query) {
-		try {
-			filters = JSON.parse(decodeURIComponent(query));
-		} catch (e) {
-			if (e instanceof SyntaxError) {
-				res.status(400).send("Bad filters parameter");
-			} else {
-				throw e;
-			}
-		}
-	}
-
-	// Get posts that match filters from database
-	const posts = await db.getPosts(filters);
-	res.send(posts);
-});
-
-app.put('/api/posts', async (req, res) => {
-	try {
-		await db.createPost(req.body);
-		res.status(200).send("Post created");
-	} catch (e) {
-		res.status(500).send("Server error when creating post");
-	}
-});
+// Handle API requests
+app.use('/api', api);
 
 module.exports = app;
