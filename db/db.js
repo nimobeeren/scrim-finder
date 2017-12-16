d323e57341214378ca2d8d5492de46e4588e3611
Nimo Beeren <nimobeeren@gmail.com>
2017-12-16 20:52:14 +0100

    Hide mongo credentials

--- db/db.js
+++ db/db.js
@@ -1,9 +1,10 @@
 const {MongoClient} = require('mongodb');
+const mongoCredentials = require('./mongoCredentials');
 
 let db;
 
 async function connect() {
-	const uri = 'mongodb://dbuser:35dJ1G1BEc0A@cluster0-shard-00-00-ii3ed.mongodb.net:27017,cluster0-shard-00-01-ii3ed.mongodb.net:27017,cluster0-shard-00-02-ii3ed.mongodb.net:27017/db?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+	const uri = mongoCredentials.uri;
 	const dbName = 'scrim-finder-db';
 
 	const client = await MongoClient.connect(uri);
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const db = require('./db');
+const db = require('./db/db');
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
