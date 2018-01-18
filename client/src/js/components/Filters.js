ae096b29fcbd0805978619c6595bf59e245ebb1e
Nimo Beeren <nimobeeren@gmail.com>
2018-01-18 14:26:00 +0100

    Add very basic filter reset button

--- client/src/js/components/Filters.js
+++ client/src/js/components/Filters.js
@@ -5,6 +5,7 @@ import MapCheckboxes from "../containers/MapCheckboxes";
 import ServerRadioButtons from "../containers/ServerRadioButtons";
 import AgeRadioButtons from "../containers/AgeRadioButtons";
 import ChevronToggle from "./ChevronToggle";
+import Button from "./Button";
 import '../../styles/Filters.css';
 
 const Filters = (props) => {
@@ -20,11 +21,17 @@ const Filters = (props) => {
 	return (
 		<div className="filters">
 			<div className="filters__content">
-				<h3>Filters</h3>
-				<ChevronToggle
-					className="chevron--right chevron--mobile"
-					pointUp={expanded}
-					onClick={onExpandedToggle}/>
+				<div className="filters__header">
+					<h3>Filters</h3>
+					<Button
+						className="btn btn--small"
+						label="Reset"
+						onClick={() => window.location = '/'}/>
+					<ChevronToggle
+						className="chevron--right chevron--mobile"
+						pointUp={expanded}
+						onClick={onExpandedToggle}/>
+				</div>
 				<div className={"filters__fields " + (expanded ? "fields--expanded" : "")}>
 					<fieldset>
 						<legend>Level</legend>
