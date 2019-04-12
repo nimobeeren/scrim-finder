f5d7d9475cdb045b008c6f2379b0d42c1d70ff74
Nimo Beeren <nimobeeren@gmail.com>
2019-04-12 10:39:43 +0200

    Add placeholder posts

--- client/src/js/actions/PostActions.js
+++ client/src/js/actions/PostActions.js
@@ -28,15 +28,65 @@ export function fetchPosts(filters = null) {
 	return async function (dispatch) {
 		dispatch(requestPosts(filters));
 
-		const query = encodeURIComponent(JSON.stringify(filters));
-		const response = await fetch('/api/posts?filters=' + query);
+		/*
+		PLACEHOLDER
+		 */
 
-		if (response.ok) {
-			const json = await response.json();
-			dispatch(receivePosts(filters, json));
-		} else {
-			// TODO: Handle failure
-			dispatch(failPosts(filters));
-		}
+		let posts = [
+			{
+				_id: 0,
+				createdAt: new Date(2019, 3, 12, 10),
+                author: "zonic",
+                body: {
+                    teamName: "Astralis",
+                    level: 3,
+                    maps: [ "inferno", "nuke" ],
+                    server: true,
+                    ip: "localhost:1234",
+                    password: "pwd"
+                },
+                replies: []
+			},
+            {
+                _id: 1,
+                createdAt: new Date(2019, 3, 12, 10, 30),
+                author: "flusha",
+                body: {
+                    teamName: "Cloud9",
+                    level: 3,
+                    maps: [ "inferno", "mirage", "overpass" ],
+                    server: false,
+                    ip: null,
+                    password: null
+                },
+                replies: []
+            },
+            {
+                _id: 2,
+                author: "nimster",
+                body: {
+                    teamName: "AETAS Esports",
+                    level: 2,
+                    maps: [ "overpass", "nuke", "train" ],
+                    server: null,
+                    ip: "localhost:1234",
+                    password: "pwd"
+                },
+                replies: []
+            }
+		];
+
+		dispatch(receivePosts(filters, posts));
+
+		// const query = encodeURIComponent(JSON.stringify(filters));
+		// const response = await fetch('/api/posts?filters=' + query);
+		//
+		// if (response.ok) {
+		// 	const json = await response.json();
+		// 	dispatch(receivePosts(filters, json));
+		// } else {
+		// 	// TODO: Handle failure
+		// 	dispatch(failPosts(filters));
+		// }
 	}
 }
